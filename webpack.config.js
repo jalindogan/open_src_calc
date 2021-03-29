77a88561043080d9a65dc1209979330f15059d56
Arvin Lin <a7lin@ucsd.edu>
2021-03-29 10:51:51 -0700

    Configure webpack execution

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    module: {
+        rules: [
+            {
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/,
+            },
+        ],
+    },
+    target: 'web'
+};
